1cb55fdfc259a40c8f9b32d0065b53da41f11642
afghan106 <afghansa106@gmail.com>
2024-12-28 13:43:20 -0800

    piece component

---
+++ client/src/chess/model/piece.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+/**
+ * inageUrl
+ * color
+ * id 
+ * -color of player
+ * -wheter or not it's your turn
+ * - if this player's turn ,color of this plauer is the same colro as this piecethen we can make this piece draggable/moveable
+ */
+
+
+const Piece = () => {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default Piece;
