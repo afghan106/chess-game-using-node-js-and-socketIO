8abd5f1347291c9da2078fdb4b21c4f13d7b44d7
afghan106 <afghansa106@gmail.com>
2024-12-28 13:44:03 -0800

    chess component

---
+++ client/src/chess/model/chess.js
@@ -0,0 +1,19 @@
+class Game{
+    constructor(playerWhite){
+        this.playerWhite=playerWhite;// bolean   
+        this.chessBoard=this.makeStartingBoard();
+    }
+    makeStartingBoard(){
+        //orientation for the current player
+        if (this.playerWhite) {
+            // for the white player
+        } else {
+            // this is for the black player
+        }
+
+        movePiece(){
+            // piece id 
+            // to [x,y]
+        }
+    }
+}
