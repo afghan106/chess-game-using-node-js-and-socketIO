28243b1cc76b0e8957f7fd418c973cc7ac0c27f5
afghan106 <afghansa106@gmail.com>
2024-12-28 13:42:57 -0800

    square js component model

---
+++ client/src/chess/model/square.js
@@ -0,0 +1,8 @@
+class Square{
+    constructor(x,y,convaCoordiantes,piece){
+        this.x=x;
+        this.y=y;
+        this.convaCoordiantes=convaCoordiantes;
+        this.piece=piece;
+    }
+}
