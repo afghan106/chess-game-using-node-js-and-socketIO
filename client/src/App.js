35a8d76e06c041add475d39ddc6096397518a129
afghan106 <afghansa106@gmail.com>
2024-12-29 01:51:34 -0800

    create the app file

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import Piece from './chess/model/piece'
+import React from 'react';
+import ChessBoard from './ChessBoard';
 
 const App = () => {
-  return (
-    <>
-<Piece/>
-    </>
-  )
-}
+    return (
+        <div>
+            <h1>Multiplayer Chess</h1>
+            <ChessBoard />
+        </div>
+    );
+};
 
-export default App
+export default App;
